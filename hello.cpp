d3ef4a4019e9c775b24e5f8382c91803071175ac
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-11-11 17:08:01 +0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//Author : Nemuel Wainaina
+//This is a simple c++ program that prints "Hello world!"  onto the screen
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello world!"<<endl;
+    return 0;
+}
