96adec41a4119ccef71da5d0bb504dc7ec6d36ce
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2022-01-05 23:26:46 +0300

    Add files via upload

---
+++ txt_with_quotes.cpp
@@ -0,0 +1,11 @@
+//Author : Nemuel Wainaina
+//How to print text on the console with quotes around it
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"\"Hello friend !\"";
+    return 0;
+}
